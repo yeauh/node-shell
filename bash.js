1e4d970eb7be6db13ef02cee22f51c56b1ba156b
alexYea <alexanderterekhov1@gmail.com>
2023-07-10 21:45:12 -0400

    Added a reference to pwd.js and pwd()

--- bash.js
+++ bash.js
@@ -1,10 +1,12 @@
 // Output a prompt
 process.stdout.write('(^C to exit)prompt > ');
+const pwd = require('./pwd');
 
 // The stdin 'data' event fires after a user types in a line
 process.stdin.on('data', (data) => {
     const cmd = data.toString().trim(); // remove the newLine
 
     process.stdout.write('\nYou typed: ' + cmd + '\n');
+    pwd();
     process.stdout.write('\prompt > ');
 });
